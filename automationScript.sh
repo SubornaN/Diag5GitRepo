a0d82d67feb170ce302b0b7cb4f56806bae37aaf
suborna <subornadnath@gmail.com>
2022-08-20 14:10:34 -0400

    edited the automation script

--- automationScript.sh
+++ automationScript.sh
@@ -2,12 +2,8 @@
 
 date=$(date +"%m%d_%M")
 
-# while (true)
-# do
 ps -eo pid,%cpu,%mem > usage_$date.txt
 git add .
 git commit -m "Process Usage Time: $date"
 git push origin main
-# sleep (30)
-# number+=1
-# done
+
