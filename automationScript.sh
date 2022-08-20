bc0a90262d642d883d849efdb7a312f3a70101d5
suborna <subornadnath@gmail.com>
2022-08-20 16:30:44 -0400

    added exact path to save generated .txt file

--- automationScript.sh
+++ automationScript.sh
@@ -2,7 +2,7 @@
 
 date=$(date +"%m%d_%M")
 
-ps -eo pid,%cpu,%mem > usage_$date.txt
+ps -eo pid,%cpu,%mem > ~/bin/diag5gitrepo/usage_$date.txt
 git add .
 git commit -m "Process Usage Time: $date"
 git push origin main
